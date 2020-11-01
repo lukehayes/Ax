d266b477a57bca14e372aa2d36970294d38f1f90
lukehayes <lukepurplehayes@gmail.com>
2020-11-01 22:07:27 +0000

    Added build.bat for windows

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+REM cl main.c glfw4.lib -I C:\Dev\Libraries\GLFW3.3\include\ /link /LIBPATH:C:\Dev\Libraries\GLFW3.3\libs /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib
+cl main.c opengl32.lib glfw3.lib -I include /EHsc /link /LIBPATH:libs /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib
+
+
