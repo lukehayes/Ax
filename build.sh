695101b93ddd902b37f94d765c88cef4f5ba494f
lukehayes <lukepurplehayes@gmail.com>
2020-11-02 14:13:48 +0000

    Added include dir explicitly to linux build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 
 
-gcc main.c -o app -L/usr/local/lib/GLFW -lglfw3 -lGLU -lGL -lX11 -lpthread -lm -ldl
+gcc main.c -o app -I include -L/usr/local/lib/GLFW -lglfw3 -lGLU -lGL -lX11 -lpthread -lm -ldl
