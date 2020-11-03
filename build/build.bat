7dec5ba4131e8ecd3c583dd3c167c43df411eea3
lukehayes <lukepurplehayes@gmail.com>
2020-11-03 11:28:58 +0000

    Windows build.bat now compiles project successfully

--- build/build.bat
+++ build/build.bat
@@ -1,5 +1,5 @@
 @echo off
 REM cl main.c glfw4.lib -I C:\Dev\Libraries\GLFW3.3\include\ /link /LIBPATH:C:\Dev\Libraries\GLFW3.3\libs /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib
-cl ../main.c User32.lib opengl32.lib glfw3.lib -I ../include /EHsc /link /LIBPATH:../libs /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib
+cl /MD ../main.c -I ../include /link glfw3.lib gdi32.lib user32.lib opengl32.lib kernel32.lib ucrt.lib shell32.lib msvcrt.lib /LIBPATH:../libs /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib
 
 
