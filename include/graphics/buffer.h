ca5fe99eb3ba9d23a9005df1906546e60eacd304
lukehayes <lukepurplehayes@gmail.com>
2020-11-04 09:56:50 +0000

    CG_CreateBuffer now adds verticies to the Buffer struct

--- include/graphics/buffer.h
+++ include/graphics/buffer.h
@@ -1,21 +1,29 @@
 #ifndef __GFX_BUFFER_H__
 #define __GFX_BUFFER_H__
 
+#include <stdio.h>
 #include "common/types.h"
 
 typedef struct Buffer
 {
-    f32* verticies;
-
+    f32 x, y;
+    f32 r, g, b;
+    f32 verticies[9];
 } Buffer;
-
 
 _inline void
 CG_CreateBuffer(Buffer* buffer)
 {
-    f32 data[] = {0.3,0.4,0.3};
+    float arr[9] = {
+       -1.0f, -1.0f, 0.0f,
+       1.0f, -1.0f, 0.0f,
+       0.0f,  1.0f, 0.0f,
+    };
 
-    buffer->verticies = data;
+    for(int i = 0; i<=9; i++)
+    {
+        buffer->verticies[i] = arr[i];
+    }
 
     printf("Creating BUffer");
 }
