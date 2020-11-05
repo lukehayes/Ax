8d43232b6d3acadac2cac6a8f5825b2215dbe77b
lukehayes <lukepurplehayes@gmail.com>
2020-11-05 14:48:01 +0000

    Updated readme

--- build/build.sh
+++ build/build.sh
@@ -5,8 +5,10 @@ SRC="main.c"
 INC_DIRS="-I include"
 LINK_DIRS="-L/usr/local/lib/GLFW"
 LIBS="-lglfw3 -lGLU -lGL -lX11 -lpthread -lm -ldl"
-OUTPUT="-o bin/app"
+OUTPUT="-o app"
 
 gcc $SRC $OUTPUT $INC_DIRS $LINK_DIRS $LIBS
+
+mv app bin
 
 ./bin/app
