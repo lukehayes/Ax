cbb178adaaac2f407452a0fd21e29049433ec3a3
lukehayes <lukepurplehayes@gmail.com>
2020-11-07 17:20:40 +0000

    Added init function pointer to engine struct

--- include/engine.h
+++ include/engine.h
@@ -6,12 +6,23 @@ typedef struct Engine
     int width;
     int height;
     const char* title;
+
+    void (*init)();
 } Engine;
+
+void init()
+{
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+}
 
 Engine CG_CreateEngine()
 {
     Engine e = { 800,600, "Game" };
+    e.init = &init;
     return e;
 }
+
+
 
 #endif
