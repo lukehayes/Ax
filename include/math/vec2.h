1a0f02ab20538aa4e865822fb95a046519a2ddc1
lukehayes <lukepurplehayes@gmail.com>
2020-11-10 14:48:10 +0000

    Comment header seperators

--- include/math/vec2.h
+++ include/math/vec2.h
@@ -1,6 +1,7 @@
 #ifndef _CG_VEC2_H_
 #define _CG_VEC2_H_
 
+/** Structs **/
 typedef struct IVec2
 {
     s32 x;
@@ -12,6 +13,8 @@ typedef struct FVec2
     f32 x;
     f32 y;
 } FVec2;
+
+/** Functions **/
 
 
 #endif
