68d312819c698f7bdf71ba1c229c54a7e50cb28d
lukehayes <lukepurplehayes@gmail.com>
2020-11-11 21:59:25 +0000

    Rearranged dependency folder

--- build/build.bat
+++ build/build.bat
@@ -10,11 +10,12 @@ REM Compiler Flags
 set compileFlags=/MD /Febin\app.exe
 
 REM Link Libraries
+set includes=-I include -I deps
 set libs=glfw3.lib gdi32.lib user32.lib opengl32.lib kernel32.lib ucrt.lib shell32.lib msvcrt.lib glew32s.lib
 set math_src=src/math/*.c
 
 
-cl main.c %compileFlags% %math_src% -I include -I deps\GLEW /link %libs% /LIBPATH:libs /LIBPATH:C:\Dev\Libraries\GLEW\Libraries /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib
+cl main.c %compileFlags% %math_src% %includes% /link %libs% /LIBPATH:libs /LIBPATH:C:\Dev\Libraries\GLEW\Libraries /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib
 
 del *.obj
 
