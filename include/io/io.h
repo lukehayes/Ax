98e82ac23df2c5ed142928a02000fbdeebbb388a
lukehayes <lukepurplehayes@gmail.com>
2020-11-12 15:25:40 +0000

    Re-implemted CG_Read_File() - now uses malloc to create data buffer

--- include/io/io.h
+++ include/io/io.h
@@ -7,17 +7,25 @@
 u8
 CG_Get_Filesize(FILE* handle)
 {
+    // Move the file pointer to the end of the file to
+    // work out the file size.
     fseek(handle, 0, SEEK_END);
     int size = ftell(handle);
     fseek(handle, 0, SEEK_SET);
     return size;
 }
 
+/**
+ * Read a file into a buffer and return it. 
+ * Buffer needs to be freed after use!
+ */
 str
-CG_Read_File(const char* filename, char* buffer)
+CG_Read_File(const char* filename)
 {
     FILE* fh = fopen(filename, "r");
     int size = CG_Get_Filesize(fh);
+
+    char* buffer = malloc(sizeof(char) * size);
 
     for(int i = 0; i<= size + 1; i++) {
         buffer[i] = fgetc(fh);
