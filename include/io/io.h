461b0ba82d6e97593d1fa9837f903c3821ff943e
lukehayes <lukepurplehayes@gmail.com>
2020-11-12 15:08:59 +0000

    Removed unused counter variable

--- include/io/io.h
+++ include/io/io.h
@@ -18,7 +18,6 @@ CG_Read_File(const char* filename, char* buffer)
 {
     FILE* fh = fopen(filename, "r");
     int size = CG_Get_Filesize(fh);
-    int counter = 0;
 
     for(int i = 0; i<= size + 1; i++) {
         buffer[i] = fgetc(fh);
