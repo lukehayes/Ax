83f4b80d97783fb8cf6fbcfe905dd15ac51dc798
lukehayes <lukepurplehayes@gmail.com>
2020-11-16 15:10:30 +0000

    Added GLFW_OPENGL_CORE_PROFILE flag with glfwWindowHint

--- include/engine.h
+++ include/engine.h
@@ -19,6 +19,7 @@ void init()
 {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
 void error_callback(int error, const char* description)
