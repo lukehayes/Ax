e821c7ae7b3999bbc9e0b4b13ca9f7f16445ca37
lukehayes <lukepurplehayes@gmail.com>
2020-12-03 21:39:48 +0000

    Updated Windows build script

--- compile/build.bat
+++ compile/build.bat
@@ -3,26 +3,29 @@ REM cl main.c glfw4.lib -I C:\Dev\Libraries\GLFW3.3\include\ /link /LIBPATH:C:\D
 
 del bin/app.exe
 
-set outputDir=bin
-MKDIR %outputDir%
+set OUTPUT_DIR=bin
+MKDIR %OUTPUT_DIR%
 
 REM Compiler Flags
-set compileFlags=/MD /Febin\app.exe
-
+set COMPILE_FLAGS=/MD /Febin\app.exe
 
 REM Link Libraries
 
 set LOCAL_LIBS_DIR=C:\Dev\Libraries
-set includes=-I include -I %LOCAL_LIBS_DIR%\GLFW\include -I %LOCAL_LIBS_DIR%\GLEW\Include -I %LOCAL_LIBS_DIR%\CGLM\Include\
+set INCLUDES=-I include -I %LOCAL_LIBS_DIR%\GLFW\include -I %LOCAL_LIBS_DIR%\GLEW\Include -I %LOCAL_LIBS_DIR%\CGLM\Include\
 set LIBS_DIR= /LIBPATH:%LOCAL_LIBS_DIR%\GLFW\Libs /LIBPATH:%LOCAL_LIBS_DIR%\GLEW\Libs /LIBPATH:%LOCAL_LIBS_DIR%\CGLM\Libs
 set LIBS=glfw3.lib gdi32.lib user32.lib opengl32.lib kernel32.lib ucrt.lib shell32.lib msvcrt.lib glew32s.lib
-set math_src=src/math/*.c
-set util_src=src/util/*.c
+
+REM Project Sources
+set SRC_ROOT=src/*.c
+set MATH_SRC=src/math/*.c
+set GFX_SRC=src/graphics/*.c
+set ALL_DRC=%SRC_ROOT% %MATH_SRC% %GFX_SRC%
 
 
-cl main.c %compileFlags% %math_src% %includes% /link %LIBS% %LIBS_DIR% /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib
+cl main.c %COMPILE_FLAGS% %ALL_SRC% %INCLUDES% /link %LIBS% %LIBS_DIR% /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib
 
 del *.obj
 
-REM bin\app.exe
+bin\app.exe
 
