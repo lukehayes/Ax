45b09bbd68fe0331802bf6a9388833af401bae8f
lukehayes <lukepurplehayes@gmail.com>
2020-12-03 18:15:56 +0000

    Buffer struct now allocating verticies correctly

--- src/graphics/buffer.c
+++ src/graphics/buffer.c
@@ -1,23 +1,21 @@
 #include "graphics/buffer.h"
 
+static float arr[9] = {
+   -1.0f, -1.0f, 0.0f,
+   1.0f, -1.0f, 0.0f,
+   0.0f,  1.0f, 0.0f,
+};
+
 
 void
 CG_CreateBuffer(Buffer* buffer)
 {
     buffer->stride = 0;
-    s32 vertexCount = 9;
-    s32 vertexBytes = sizeof(float) * vertexCount;
-    buffer->vertexCount = vertexCount;
-    buffer->vertexBytes = vertexBytes;
+    buffer->vertexCount = 9;
+    buffer->vertexBytes = sizeof(float) * (buffer->vertexCount);
 
-    float arr[9] = {
-       -1.0f, -1.0f, 0.0f,
-       1.0f, -1.0f, 0.0f,
-       0.0f,  1.0f, 0.0f,
-    };
-
-    buffer->verticies = malloc(vertexBytes);
-    memcpy(buffer->verticies, arr, sizeof(vertexBytes));
+    buffer->verticies = malloc(buffer->vertexBytes);
+    memcpy(buffer->verticies, arr, buffer->vertexBytes);
 }
 
 void
