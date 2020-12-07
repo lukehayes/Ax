a85b609bab590db8261f64b8a712d6823c1a9e68
lukehayes <lukepurplehayes@gmail.com>
2020-12-07 11:16:33 +0000

    Added id member to buffer struct

--- src/graphics/buffer.c
+++ src/graphics/buffer.c
@@ -6,10 +6,12 @@ static float arr[9] = {
    0.0f,  1.0f, 0.0f,
 };
 
+static u8 id = 1;
 
 void
 CG_CreateBuffer(Buffer* buffer)
 {
+    buffer->id = id;
     buffer->stride = 0;
     buffer->vertexCount = 9;
     buffer->vertexBytes = sizeof(float) * (buffer->vertexCount);
