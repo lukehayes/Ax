c09725973a9ee211d930d6526f09a2264f8e3bd7
lukehayes <lukepurplehayes@gmail.com>
2020-12-12 18:39:47 +0000

    CubeModel struct simplified and ready to be implemented correctly

--- include/model/cube-model.h
+++ include/model/cube-model.h
@@ -2,54 +2,17 @@
 #define CUBE_MODEL_H
 
 #include "engine.h"
+#include "math/cglm-all.h"
+
 
 /* #####   TYPE DEFINITIONS   ###################################################### */
 
-#define BUFFER_SIZE 108
-
-
 typedef struct CubeModel 
 {
-    s8 id;
-    s8 stride;
-    s8 count;
-    s32 vertexCount;
-    s32 vertexBytes;
-    float verticies[108];
-
-    // OpenGL Specific Types
-    GLuint VAO_ID;
-    GLuint VBO_ID;
-
+    mat4 modelMatrix;
+    vec3 position;
     
 } CubeModel;
-
-
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  CG_Generate_Buffers
- *  Description:  Generate the VAO and VBO for this model
- * =====================================================================================
- */
-void CG_Generate_Buffers(CubeModel* model);
-
-/* #####   FUNCTION DEFINITIONS  -   ############################################### */
-
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  CreateModelCube
- *  Description:  Create an instance of a cube model.
- * =====================================================================================
- */
-void CG_CreateModelCube(CubeModel* model);
-
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  DestroyCubeModel
- *  Description:  Free all memory that the Cube Model struct holds.
- * =====================================================================================
- */
-void CG_DestroyModelCube(CubeModel* model);
 
 
 #endif
