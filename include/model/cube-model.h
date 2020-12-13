4f416eb88189f3a844051707ef914b7ef914a98d
lukehayes <lukepurplehayes@gmail.com>
2020-12-13 18:36:15 +0000

    Renamed modelMatrix member to model in CubeModel struct

--- include/model/cube-model.h
+++ include/model/cube-model.h
@@ -1,18 +1,17 @@
 #ifndef CUBE_MODEL_H
 #define CUBE_MODEL_H
 
-#include "engine.h"
 #include "math/cglm-all.h"
-
 
 /* #####   TYPE DEFINITIONS   ###################################################### */
 
 typedef struct CubeModel 
 {
-    mat4 modelMatrix;
+    mat4 matrix;
     vec3 position;
     
 } CubeModel;
 
+void CG_CreateCubeModel(CubeModel* self);
 
 #endif
