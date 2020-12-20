5afb51926ab920e90632f9b85850cc601e550630
lukehayes <lukepurplehayes@gmail.com>
2020-12-20 17:48:04 +0000

    CG_CreateCubeModel now creating model/initializing array correctly

--- src/model/cube-model.c
+++ src/model/cube-model.c
@@ -1,10 +1,14 @@
 #include "model/cube-model.h"
 #include "util/log.h"
+#include <string.h>
 
 void CG_CreateCubeModel(CubeModel* self)
 {
-	self->matrix = GLM_MAT4_IDENTITY_INIT;
-	self->position = GLM_VEC3_ZERO_INIT;
+    mat4 mat = GLM_MAT4_IDENTITY_INIT;
+    memcpy(self->matrix, &mat, sizeof(float) * 16);
+
+    vec3 pos = GLM_VEC3_ZERO_INIT;
+    memcpy(self->position, &pos, sizeof(float) * 3);
 }
 
 
