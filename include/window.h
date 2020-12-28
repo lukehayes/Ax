f46db6fc2d69478f2bd73c84164fa77bb3e21000
lukehayes <lukepurplehayes@gmail.com>
2020-12-28 17:43:23 +0000

    Created src/window.cpp file

--- include/window.h
+++ include/window.h
@@ -1,33 +1,20 @@
 #ifndef CG_WINDOW_H
 #define CG_WINDOW_H
 
+#include <GLEW/glew.h>
+#include <GLFW/glfw3.h>
+#include "common/types.h"
 namespace CG 
 {
-
     class Window
     {
         public:
-            Window(){}
-            ~Window(){}
+            Window();
+            ~Window();
 
-            void Create()
-            {
-                glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-                glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-                glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+            void Create();
 
-                this->window = glfwCreateWindow(this->width, this->height, this->title, NULL, NULL);
-
-                glfwMakeContextCurrent(this->window);
-
-                /* Glew MUST be initialized after context creation*/
-                GLenum err = glewInit();
-
-                if( GLEW_OK != err )
-                {
-                    fprintf(stderr, "Error: %s \n", glewGetErrorString(err));
-                }
-            }
+            GLFWwindow* GetWindow();
 
             s32 width = 800;
             s32 height = 600;
---
+++ src/window.cpp
@@ -0,0 +1,31 @@
+#include "window.h"
+
+namespace CG
+{
+
+    Window::Window() {}
+    Window::~Window() {}
+
+    void
+    Window::Create()
+    {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        this->window = glfwCreateWindow(this->width, this->height, this->title, NULL, NULL);
+
+        glfwMakeContextCurrent(this->window);
+
+        /* Glew MUST be initialized after context creation*/
+        GLenum err = glewInit();
+
+        if( GLEW_OK != err )
+        {
+            fprintf(stderr, "Error: %s \n", glewGetErrorString(err));
+        }
+    }
+
+    GLFWwindow* 
+    Window::GetWindow() { return this->window; }
+}
