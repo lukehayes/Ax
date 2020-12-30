180b428ece9010c8e838d05924e0750b39da6618
lukehayes <lukepurplehayes@gmail.com>
2020-12-30 20:30:41 +0000

    Core::Transform now a C++ class

--- include/core/transform.h
+++ include/core/transform.h
@@ -1,17 +1,16 @@
 #ifndef CORE_TRANSFORM_H
 #define CORE_TRANSFORM_H
 
-#include "common/types.h"
-#include "math/cglm-all.h"
-
-typedef struct Transform
+namespace Core
 {
-    vec3 position;
-    vec3 rotation;
-    vec3 scale;
-} Transform;
 
-void
-CG_CreateTransform(Transform* self);
+    class Transform
+    {
+        public:
+            Transform(){}
+            ~Transform(){}
+    };
+}
+
 
 #endif
