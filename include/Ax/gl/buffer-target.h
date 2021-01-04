cbb33ef9cd5a2fed587fc5e887cacdba5399d832
lukehayes <lukepurplehayes@gmail.com>
2021-01-04 16:51:07 +0000

    Added Ax::GL::BufferTarget enum

---
+++ include/Ax/gl/buffer-target.h
@@ -0,0 +1,17 @@
+#ifndef BUFFER_TARGET_H
+#define BUFFER_TARGET_H
+
+#include <GLEW/glew.h>
+
+namespace Ax::GL
+{
+    enum BufferTarget
+    {
+        ARRAY_BUFFER = GL_ARRAY_BUFFER,
+        ELEMENT_BUFFER = GL_ELEMENT_ARRAY_BUFFER,
+        TEXTURE_BUFFER = GL_TEXTURE_BUFFER
+    };
+
+}
+
+#endif
