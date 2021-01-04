da15b22ec00fbcffe46e91e916839660b6721904
lukehayes <lukepurplehayes@gmail.com>
2021-01-04 13:18:30 +0000

    Added common/glm.h to project

---
+++ include/common/glm.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_GLM_H
+#define COMMON_GLM_H
+
+// All of the GLM functionality needed are are found in these headers:
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#endif
