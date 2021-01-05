d7ed7f10e917ae1e002f235c5475746f5fa8c49a
lukehayes <lukepurplehayes@gmail.com>
2021-01-05 18:10:05 +0000

    Main.cpp now using simple Camera matrix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Ax/engine.h"
 #include "Ax/gl/vertex-array.h"
 #include "Ax/gl/buffer-target.h"
+#include "Ax/gl/buffer-config.h"
 #include "Ax/gl/shader.h"
 
 int main(int argc, const char *argv[])
@@ -16,41 +17,81 @@ int main(int argc, const char *argv[])
         "assets/shaders/FSH-Default.glsl"
     );
 
-    vao.addData({
-            0.0f, 1.0f, 0.9f,
-            -1.0f, -1.0f, 0.9f,
-            1.0f, -1.0f, -0.5f,
-            }, Ax::GL::BufferTarget::ARRAY_BUFFER);
+    Ax::GL::BufferConfig config{0,2,0, Ax::GL::ARRAY_BUFFER};
 
-    std::cout << Engine.window().width() << std::endl;
+    vao.setBufferData({
+           -1.0,   1.0,
+           -1.0,  -1.0,
+            1.0,   1.0,
+            1.0,  -1.0,
+            }, config);
+    vao.setAttribPointers(config);
+
+    Ax::GL::VertexArray vao2;
+    vao2.setBufferData({
+           -1.0,   1.0,
+           -1.0,  -1.0,
+            1.0,   1.0,
+            1.0,  -1.0,
+            }, config);
+    vao2.setAttribPointers(config);
+
+    glm::mat4 projection = glm::perspective(
+            glm::radians(45.0f),
+            4.0f/ 3.0f,
+            0.1f,
+            100.0f
+    );
+
+    glm::mat4 view = glm::lookAt(
+            glm::vec3(0.0f, 0.0f, -3.0f),
+            glm::vec3(0.0f, 0.0f,0.0f),
+            glm::vec3(0.0f, 1.0f,0.0f)
+    );
+
+    glm::mat4 model = glm::mat4(1.0f);
+
+    //glm::translate(model, glm::vec3(0.0f, 0.0f, -40.0f));
+
+    float c = 0.0;
+
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(Engine.window().window() ))
 	{
-		//glfwGetFramebufferSize(Engine.getWindow().window, Engine.getWindow().width, &Engine.getWindow().height);
 
-		//glViewport(0, 0, width, height);
-        //glClearColor(0.0,0.3f,1.0f,1.0f);
-		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		//glUseProgram(shader.program);
-        //glBindVertexArray(mesh.VAO_ID);
-    
-        shader.use();
-        vao.bind();
-
-        //glUniformMatrix4fv(glGetUniformLocation(shader.program, "projection"), 1, GL_FALSE, (float*)camera.projection);
-		//glUniformMatrix4fv(glGetUniformLocation(shader.program, "view"), 1, GL_FALSE, (float*)camera.view);
-        //glUniformMatrix4fv(glGetUniformLocation(shader.program, "model"), 1, GL_FALSE, (float*)model.matrix);
-        
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(Engine.window().window());
+        c += 0.1;
 
 		/* Poll for and process events */
 		glfwPollEvents();
-        /*CG_DestroyModelCube(&cube);*/
+
+		//glfwGetFramebufferSize(Engine.window().window, Engine.window().width, &Engine.window().height);
+
+		//glViewport(0, 0, width, height);
+        glClearColor(0.0,0.0f,0.0f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+        shader.use();
+        shader.setMat4("projection", projection);
+        shader.setMat4("view", view);
+
+        vao.bind();
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model,  glm::vec3(std::cos(c), std::sin(c), 11.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+        shader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 8);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model,  glm::vec3(-2.0f, 0.0f, 30.0f + std::sin(c) * 11.0f));
+        model = glm::rotate(model, glm::radians(c * 100), glm::vec3(1.0f, 1.0f, 1.0f));
+        shader.setMat4("model", model);
+        glDrawArrays(GL_LINE_STRIP, 0, 8);
+
+
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(Engine.window().window());
 	}
 
     Engine.shutdown();
