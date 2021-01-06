68465b00838490c5171c068543128ac237c1837b
lukehayes <lukepurplehayes@gmail.com>
2021-01-06 17:53:54 +0000

    Added position, rotation and scale to Core::Transform class

--- include/Ax/System/Core/Transform.h
+++ include/Ax/System/Core/Transform.h
@@ -1,6 +1,8 @@
 #ifndef AX_SYS_CORE_TRANSFORM_H
 #define AX_SYS_CORE_TRANSFORM_H
 
+#include "Ax/System/Common/Types.h"
+
 namespace Core
 {
 
@@ -9,8 +11,21 @@ namespace Core
         public:
             Transform(){}
             ~Transform(){}
+
+        private:
+            V3 m_position;
+            V3 m_rotation;
+            V3 m_scale;
     };
 }
 
+inline V3
+Core::Transorm::position() { return this->m_position; }
+
+inline V3
+Core::Transorm::rotation() { return this->m_rotation; }
+
+inline V3
+Core::Transorm::scale() { return this->m_scale; }
 
 #endif
