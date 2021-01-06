24b3e443abc971166724e42fb987fafb80ac9eba
lukehayes <lukepurplehayes@gmail.com>
2021-01-06 17:00:27 +0000

    Added GL::Primitive enum

---
+++ include/Ax/System/GL/Primitive.h
@@ -0,0 +1,23 @@
+#ifndef AX_SYS_GL_PRIMITIVE_H
+#define AX_SYS_GL_PRIMITIVE_H
+
+#include <GLEW/glew.h>
+
+namespace Ax::System::GL
+{
+    /**
+     * Enum that defines all of the primitive rendering types.
+     */
+    enum Primitive
+    {
+        POINTS         = GL_POINTS,
+        TRIANGLES      = GL_TRIANGLES,
+        LINES          = GL_LINES,
+        LINE_STRIP     = GL_LINE_STRIP,
+        LINE_LOOP      = GL_LINE_LOOP,
+        TRIANGLE_FAN   = GL_TRIANGLE_FAN
+        TRIANGLE_STRIP = GL_TRIANGLE_STRIP
+    };
+}
+
+#endif
