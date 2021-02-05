313b714df307b2eba0383c7195f9475fe6e56666
lukehayes <lukepurplehayes@gmail.com>
2021-02-05 18:46:44 +0000

    Camera3D default position now {0,0,0}

--- src/Ax/System/Graphics/Camera3D.cpp
+++ src/Ax/System/Graphics/Camera3D.cpp
@@ -12,7 +12,7 @@ namespace Ax::System::Graphics {
                 )),
 
           view( glm::lookAt(
-                glm::vec3(0.0f, 0.0f, 3.0f),
+                glm::vec3(0.0f, 0.0f, 0.0f),
                 glm::vec3(0.0f, 0.0f, -1.0f),
                 glm::vec3(0.0f, 1.0f,0.0f)
                 ))
