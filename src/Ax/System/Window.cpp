2c4d582b9cf5ed485694920d21938cb7f42ebde7
lukehayes <lukepurplehayes@gmail.com>
2021-03-01 15:51:40 +0000

    Removed old GLEW initialization code from Window.cpp

--- src/Ax/System/Window.cpp
+++ src/Ax/System/Window.cpp
@@ -16,20 +16,10 @@ namespace Ax::System
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
+        
         m_window = glfwCreateWindow(m_width, m_height, m_title, NULL, NULL);
-
         glfwMakeContextCurrent(m_window);
 
         gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
-
-        /* Glew MUST be initialized after context creation*/
-        //GLenum err = glewInit();
-
-        //if( GLEW_OK != err )
-        //{
-            //fprintf(stderr, "Error: %s \n", glewGetErrorString(err));
-        //}
     }
 }
