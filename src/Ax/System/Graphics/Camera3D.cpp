4868dcd99313256a51dac1784e333fddced09c16
lukehayes <lukepurplehayes@gmail.com>
2021-03-02 13:43:40 +0000

    Added transform member to ctor initializer list

--- src/Ax/System/Graphics/Camera3D.cpp
+++ src/Ax/System/Graphics/Camera3D.cpp
@@ -4,7 +4,9 @@
 namespace Ax::System::Graphics {
 
     Camera3D::Camera3D()
-        : projection(glm::perspective(
+        : 
+            transform(),
+            projection(glm::perspective(
             glm::radians(45.0f),
             800.0f / 600.0f,
             0.1f,
@@ -12,7 +14,7 @@ namespace Ax::System::Graphics {
                 )),
 
           view( glm::lookAt(
-                glm::vec3(0.0f, 0.0f, 0.0f),
+                this->transform.position(),
                 glm::vec3(0.0f, 0.0f, -1.0f),
                 glm::vec3(0.0f, 1.0f,0.0f)
                 ))
@@ -23,12 +25,19 @@ namespace Ax::System::Graphics {
     void   
     Camera3D::update(double dt) 
     {
+        static float c = 0.0f;
         float x = this->transform.position().x;
         float y = this->transform.position().y;
         float z = this->transform.position().z;
 
+        c += 0.1;
+
+        this->transform.position().x = std::cos(c) * 200.0f;
+        this->transform.position().y = std::cos(c) * 200.0f;
+        this->transform.position().z = std::sin(c) * 200.0f;
+
         this->view = glm::lookAt(
-            glm::vec3(x,y,z),
+            this->transform.position(),
             glm::vec3(0.0f, 0.0f, -1.0f),
             glm::vec3(0.0f, 1.0f,0.0f)
         );
