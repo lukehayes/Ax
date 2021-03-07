56e084a60e9a99a4fcfa99a6335d85b1457dbd37
lukehayes <lukepurplehayes@gmail.com>
2021-03-07 17:31:08 +0000

    Added b8 typedef for bool

--- include/Ax/System/Common/Types.h
+++ include/Ax/System/Common/Types.h
@@ -12,6 +12,7 @@
 // Primitives
 ============================================================*/
 using u8        =       uint8_t;
+using b8        =       u8;
 using s8        =       int8_t;
 using u16       =       uint16_t;
 using s16       =       int16_t;
