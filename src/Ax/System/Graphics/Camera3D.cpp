6641ccdd0db233a29c1ae0bc400b44198b508441
lukehayes <lukepurplehayes@gmail.com>
2021-03-10 15:12:46 +0000

    Added corrected super class inheritance in Camera3D class

--- src/Ax/System/Graphics/Camera3D.cpp
+++ src/Ax/System/Graphics/Camera3D.cpp
@@ -6,32 +6,36 @@ namespace Ax::System::Graphics {
     Camera3D::Camera3D()
         :
             Camera(
-            glm::perspective(
-                    glm::radians(45.0f),
-                    800.0f / 600.0f,
-                    0.1f,
-                    1000.0f
-                    ),
+                    glm::perspective(
+                        glm::radians(45.0f),
+                        800.0f / 600.0f,
+                        0.1f,
+                        1000.0f
+                        ),
 
-            glm::lookAt(
-                    this->transform.position(),
-                    glm::vec3(0.0f, 0.0f, -1.0f),
-                    glm::vec3(0.0f, 1.0f,0.0f)
-                    ),
+                    glm::lookAt(
+                        this->transform.position(),
+                        glm::vec3(0.0f, 0.0f, -1.0f),
+                        glm::vec3(0.0f, 1.0f,0.0f)
+                        ),
 
-            {0.0f, 0.0f, -10.0f}
-            ){}
+                    {0.0f, 0.0f, -10.0f}
+                  ){}
 
     Camera3D::~Camera3D(){}
 
-    void   
-    Camera3D::update() 
+    /**
+     * Update the objects view or projection matrix.
+     *
+     * @return void
+     */
+    void Camera3D::update() 
     {
         this->view = glm::lookAt(
                 this->transform.position(),
                 glm::vec3(0.0f, 0.0f, -1.0f),
                 glm::vec3(0.0f, 1.0f,0.0f)
-                );
+            );
     }
 
 } /* namespace Ax::System::Graphics */
