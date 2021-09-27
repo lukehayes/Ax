625ee12074b7a2bc5eac943ab665febb17b03b95
lukehayes <lukepurplehayes@gmail.com>
2021-09-27 17:21:37 +0100

    Added render test squares inside main loop.

--- include/Ax/System/Game.h
+++ include/Ax/System/Game.h
@@ -46,17 +46,23 @@ class Game
                 //---------------------------------------------------------------------
 
                 Ax::System::Graphics::Camera2D camera2d;
-
                 this->renderer.setCamera(&camera2d);
 
                 camera2d.update();
+                static float c = 0.0f;
+                c += 0.001;
 
+                //this->renderer.draw(0,1, {0,1,1});
+                //this->renderer.draw(-200,200, {1,0,0 });
+                //this->renderer.draw(-300,200, {1,0,0});
+                //this->renderer.draw(-400,200 + (std::sin(c) * 100), {1,1,0});
 
-                for (int i = 0; i < 10; i++) {
-                    for (int j = 0; j < 10; j++) {
-                        this->renderer.draw(i * 10, j * 10);
+                for (int i = 0; i < 100; i++) {
+                    for (int j = 0; j < 100; j++) {
+                        this->renderer.draw(i * 10,j * 10,{0,0.1,1});
                     }
                 }
+
 
                 //Ax::System::Graphics::Camera3D camera;
                 //camera.update();
@@ -66,7 +72,7 @@ class Game
 
                 //for (int i = 0; i < 10; i++) {
                     //for (int j = 0; j < 10; j++) {
-                        //this->renderer.draw(i * 20, j * 20);
+                        //this->renderer.draw(i * 20, j * 20, {0.5,0,1});
                     //}
                 //}
 
