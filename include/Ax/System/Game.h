f481cb26b8fb86511d36e2d0573b6106a62ca5ff
lukehayes <lukepurplehayes@gmail.com>
2021-10-08 10:18:00 +0100

    Added different test drawings.

--- include/Ax/System/Game.h
+++ include/Ax/System/Game.h
@@ -4,6 +4,7 @@
 #include "Ax/System/Common/Types.h"
 #include "Ax/System/Engine.h"
 #include "Ax/System/Mesh/MeshRenderer.h"
+#include "Ax/System/Math/Random.h"
 
 class Game
 {
@@ -48,18 +49,34 @@ class Game
                 Ax::System::Graphics::Camera2D camera2d;
                 this->renderer.setCamera(&camera2d);
 
+
                 camera2d.update();
                 static float c = 0.0f;
-                c += 0.001;
+                c += 0.0001;
 
                 //this->renderer.draw(0,1, {0,1,1});
                 //this->renderer.draw(-200,200, {1,0,0 });
                 //this->renderer.draw(-300,200, {1,0,0});
                 //this->renderer.draw(-400,200 + (std::sin(c) * 100), {1,1,0});
 
-                for (int i = 0; i < 100; i++) {
-                    for (int j = 0; j < 100; j++) {
-                        this->renderer.draw(i * 10,j * 10,{0,0.1,1});
+                for (int i = 0; i < 10; i++) {
+                    for (int j = 0; j < 10; j++) {
+                        M4 model = glm::scale(model,  glm::vec3(10.0f));
+                        this->renderer.getShader().setMat4("model", model);
+                        this->renderer.draw(i * 100.0, j * 100.0,{1,0.1,1});
+                    }
+                }
+
+                Ax::System::Graphics::Camera3D camera3d;
+                this->renderer.setCamera(&camera3d);
+                camera3d.update();
+
+                for (int i = 0; i < 10; i++) {
+                    for (int j = 0; j < 10; j++) {
+                        float r = Ax::System::Math::Random::randDouble(0,1);
+                        float g = Ax::System::Math::Random::randDouble(0,1);
+                        float b = Ax::System::Math::Random::randDouble(0,1);
+                        this->renderer.draw(i * 10,j * 10,{r,g,b});
                     }
                 }
 
