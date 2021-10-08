8e8ff87c21431507e2c83f4eb5b56ca49c023cd5
lukehayes <lukepurplehayes@gmail.com>
2021-10-08 09:46:37 +0100

    Q key now closed window exactly as ESC does.

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@
 #include <memory>
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){    
-   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+   if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_PRESS){
        glfwSetWindowShouldClose(window, GL_TRUE); 
    }
 }
