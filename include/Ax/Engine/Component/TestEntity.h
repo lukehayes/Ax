08c54467f78a8f8b1b98bd0c71e68ba73aa5b056
lukehayes <lukepurplehayes@gmail.com>
2021-10-17 14:28:39 +0100

    TestEntity::update() now contains position code.

--- include/Ax/Engine/Component/TestEntity.h
+++ include/Ax/Engine/Component/TestEntity.h
@@ -7,19 +7,21 @@ namespace Ax::Engine::Component {
 
     class TestEntity
     {
-    public:
-                    TestEntity() {}
-                    ~TestEntity() {}
+        public:
+            TestEntity() {}
+            ~TestEntity() {}
 
-        void update(double dt)
-        {
-            //transform.position.x = 790;
-            //transform.position.y = 590;
-            //transform.position.z = -5.0;
-        }
+            void update(double dt)
+            {
+                static float c = 0.0;
+                c += 0.0001;
+                transform.position.x = std::cos(c) * 10.0f;
+                transform.position.y = std::sin(c) * 10.0f;
+                transform.position.z = -100.0f + std::cos(c) * 10.0f;
+            }
 
-        Transform    transform;
-        V3           color = {0,0,0};
+            Transform    transform;
+            V3           color = {0,0,0};
     };
 }
 
