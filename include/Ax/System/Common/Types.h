6f9b78107d0e0070da38dfb5b7b83f3e9cc20100
lukehayes <lukepurplehayes@gmail.com>
2021-10-22 16:16:52 +0100

    Added const_str_ref typedef to Types.h.

--- include/Ax/System/Common/Types.h
+++ include/Ax/System/Common/Types.h
@@ -12,20 +12,21 @@
 /*============================================================
 // Primitives
 ============================================================*/
-using u8        =       uint8_t;
-using b8        =       u8;
-using s8        =       int8_t;
-using u16       =       uint16_t;
-using s16       =       int16_t;
-using u32       =       uint32_t;
-using s32       =       int32_t;
-using b32       =       s32;
-using u64       =       uint64_t;
-using s64       =       int64_t;
-using f32       =       float;
-using f64       =       double;
-using str       =       std::string;
-using const_str =       const char*;
+using u8            = uint8_t;
+using b8            = u8;
+using s8            = int8_t;
+using u16           = uint16_t;
+using s16           = int16_t;
+using u32           = uint32_t;
+using s32           = int32_t;
+using b32           = s32;
+using u64           = uint64_t;
+using s64           = int64_t;
+using f32           = float;
+using f64           = double;
+using str           = std::string;
+using const_str_ref = const std::string&;
+using const_str     = const char*;
 
 /*============================================================
 // Template Typedefs
