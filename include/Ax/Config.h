5476f2f69143b466e085ffd8b97553a87c3a2071
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-21 17:24:16 +0000

    Added Ax/Config.h to project.

---
+++ include/Ax/Config.h
@@ -0,0 +1,18 @@
+#ifndef AX_CONFIG_H
+#define AX_CONFIG_H
+
+#include <string>
+
+/**
+ * Location for all global settings for simplicity.
+ */
+struct Config
+{
+    int width =  800;
+    int height = 600;
+    std::string title = "No Game Title";
+};
+
+Config config;
+
+#endif
