e2941046f6d00d3998922b93a0b4a859529ba059
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-22 10:30:30 +0000

    Added Ax::GL::BufferConfig struct to project.

---
+++ include/Ax/GL/BufferConfig.h
@@ -0,0 +1,21 @@
+#ifndef AX_BUFCONFIG_H
+#define AX_BUFCONFIG_H
+
+#include "Ax/GL/BufferTarget.h"
+#include "Ax/GL/Primitive.h"
+#include "Ax/Common/Types.h"
+
+namespace Ax::GL {
+
+    struct BufferConfig
+    {
+        s32 attributePosition;
+        s32 vertexSize;
+        s32 vertexStride;
+        BufferTarget target;
+        Primitive primitive;
+    };
+}
+
+
+#endif
