fc34d563b08f220377193e21582a958bc8e3c9b2
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-22 13:42:27 +0000

    Changed Config.title from std::string to const char*.

--- include/Ax/Config.h
+++ include/Ax/Config.h
@@ -1,8 +1,6 @@
 #ifndef AX_CONFIG_H
 #define AX_CONFIG_H
 
-#include <string>
-
 /**
  * Location for all global settings for simplicity.
  */
@@ -10,7 +8,7 @@ struct Config
 {
     int width =  800;
     int height = 600;
-    std::string title = "No Game Title";
+    const char* title = "No Game Title";
 };
 
 #endif
