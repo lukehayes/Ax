878a682ea673481ec6128e437bf484c1c44d7950
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-22 20:25:19 +0000

    Added simple VAO,VBO and shader functionality to main

--- main.cpp
+++ main.cpp
@@ -28,16 +28,80 @@ int main(int argc, const char *argv[])
 {
     Ax::System::Window window;
 
-    Ax::GL::VertexArray vao;
+    glm::mat4 projection = glm::mat4(1.0);
+    glm::mat4 view = glm::mat4(1.0);
+    glm::mat4 model = glm::mat4(1.0);
+    glm::translate(model, {0,0,-100});
 
+
+    projection = glm::perspective(
+        glm::radians(45.0f),
+        800.0f / 600.0f,
+        0.1f,
+        1000.0f
+        );
+
+    view = glm::lookAt(
+        glm::vec3(0.0f, 0.0f, 3.0f),
+        glm::vec3(0.0f, 0.0f, -1.0f),
+        glm::vec3(0.0f, 1.0f,0.0f)
+        );
+
+
+    Ax::GL::BufferConfig config;
+    config.attributePosition = 0;
+    config.vertexSize = 3;
+    config.vertexStride = 0;
+    config.target = Ax::GL::BufferTarget::ARRAY_BUFFER;
+
+    Ax::GL::VertexArray vao;
+    vao.generate();
+    vao.bind();
+
+
+    Ax::GL::VertexBuffer vbo;
+    vbo.config = config;
+    vbo.generate();
+    vbo.bind(Ax::GL::BufferTarget::ARRAY_BUFFER);
+    vbo.setBufferData(verticies);
+    vbo.setAttribPointers();
+
+
+    Ax::GL::Shader shader(
+        "assets/shaders/VSH-Default.glsl",
+        "assets/shaders/FSH-Default.glsl"
+    );
+
+    shader.setMat4("model", model);
+    shader.setMat4("projection", projection);
+    shader.setMat4("view", view);
+
+    static float c = 0.0;
+    model = glm::translate(model, {0,0,-200});
+    
     while (!glfwWindowShouldClose(window.getWindow()))
     {
         /* Poll for and process events */
         glfwPollEvents();
 
+        c += 0.01;
 
-        glClearColor(0.1f,0.1f,0.2f,1.0f);
+        shader.use();
+        shader.setVec3("color", {0.4,0.7,0.3});
+        model = glm::rotate(model, glm::radians(std::sin(c)), {1,1,1});
+        //glm::translate(model, {std::cos(c) * 100, std::sin(c) * 100, -std::sin(c) * 100});
+        
+        //std::cout << glm::to_string(model) << std::endl;
+
+        shader.setMat4("projection", projection);
+        shader.setMat4("view", view);
+        shader.setMat4("model", model);
+
+        glClearColor(0.1f,0.1f,0.1f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+        glDrawArrays(GL_LINE_STRIP, 0,verticies.size());
 
         glfwSwapBuffers(window.getWindow());
         glfwSetKeyCallback(window.getWindow(), key_callback);
