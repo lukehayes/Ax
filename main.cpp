de48d002cc5a7fbda76f85a20e1d147b60733e13
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-22 10:32:36 +0000

    Updated main.cpp with latest code.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
-
 #include "Ax/Common/Types.h"
 #include "Ax/Window.h"
+#include "Ax/GL/VertexArray.h"
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){    
     if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_PRESS){
-    glfwSetWindowShouldClose(window, GL_TRUE); 
+        glfwSetWindowShouldClose(window, GL_TRUE); 
     }
+
+    std::cout << "Key" << std::endl;
 }
 
-void framebuffer_callback(GLFWwindow* window, int width, int height){    
-glViewport(0,0, width, height);
+void framebuffer_callback(GLFWwindow* window, int width, int height){
+    glViewport(0,0, width, height);
 }
 
 int main(int argc, const char *argv[])
 {
     Ax::System::Window window;
+
+    Ax::GL::VertexArray vao;
 
     while (!glfwWindowShouldClose(window.getWindow()))
     {
@@ -29,7 +33,6 @@ int main(int argc, const char *argv[])
         glfwSetKeyCallback(window.getWindow(), key_callback);
     }
 
-    glfwSetKeyCallback(window.getWindow(), key_callback);
 
     return 0;
 }
