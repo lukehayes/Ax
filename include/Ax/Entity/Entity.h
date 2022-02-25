c48d30dd02e917483a5c56b9567c678e857296e0
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-25 10:35:17 +0000

    Added Entity header to project.

---
+++ include/Ax/Entity/Entity.h
@@ -0,0 +1,20 @@
+#ifndef AX_ENTITY_H
+#define AX_ENTITY_H
+
+#include "Ax/Common/Types.h"
+
+namespace Ax::Entity
+{
+    class Entity
+    {
+        public:
+            Entity() {}
+            Entity(const glm::vec3& pos) : position(pos) {}
+            ~Entity() {}
+
+            glm::vec3 position = {0,0,0};
+    };
+};
+
+
+#endif // AX_ENTITY_H
