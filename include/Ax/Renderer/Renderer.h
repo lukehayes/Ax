f4a3d30bed53f604ec965ffe0d846dd003b6b73a
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-26 09:57:46 +0000

    Removed Renderer::draw() method in favor of Renderer::drawCube()

--- include/Ax/Renderer/Renderer.h
+++ include/Ax/Renderer/Renderer.h
@@ -24,7 +24,6 @@ namespace Ax::Renderer
             this->meshes.push_back(m);
         }
 
-        void draw(Ax::Entity::Entity entity, const Ax::GL::Shader& shader, double x, double y)
         void drawCube(Ax::Entity::Entity& entity, const Ax::GL::Shader& shader, double x, double y)
         {
             glm::mat4 model = glm::mat4(1.0f);
@@ -36,39 +35,6 @@ namespace Ax::Renderer
             shader.setMat4("model", model);
             shader.setVec3("color", entity.color);
             glDrawArrays(m->vbo.config.primitive, 0, m->vertexCount);
-        }
-        {
-
-            entity.position.x = x / 100.0;
-            entity.position.y = y / 100.0;
-
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, entity.position);
-            model = glm::rotate(model, glm::radians((float)x), {1,1,1});
-
-            std::shared_ptr<Ax::Mesh::Mesh> m = meshes[1];
-            m->vao.bind();
-            shader.setMat4("model", model);
-            glDrawArrays(m->primitive, 0, m->vertexCount);
-
-            shader.setVec3("color", {0.1,0.7,0.3});
-            model = glm::mat4(1.0f);
-            entity.position.x = 0.0f;
-            entity.position.y = 1.0f;
-            model = glm::translate(model, entity.position);
-
-            m = meshes[0];
-            m->vao.bind();
-            shader.setMat4("model", model);
-            glDrawArrays(m->primitive, 0, m->vertexCount);
-
-            //for(auto mesh : meshes)
-            //{
-                //shader.setMat4("model", model);
-                //mesh->vao.bind();
-                //glDrawArrays(mesh->primitive, 0, mesh->vertexCount);
-                ////mesh->vao.unbind();
-            //}
         }
 
         std::vector<std::shared_ptr<Ax::Mesh::Mesh>> meshes;
