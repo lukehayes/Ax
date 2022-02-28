ab6208a324b9b79d89f80b3428b6263ad2e1dda8
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-28 10:58:48 +0000

    Added Ax::Mesh::RectangleMesh to project.

---
+++ include/Ax/Mesh/RectangleMesh.h
@@ -0,0 +1,59 @@
+#ifndef AX_RECTANGLE_MESH_H
+#define AX_RECTANGLE_MESH_H
+
+#include "Ax/Common/Types.h"
+#include "Ax/Mesh/Mesh.h"
+
+namespace Ax::Mesh
+{
+    class RectangleMesh : public Ax::Mesh::Mesh
+    {
+        public:
+            RectangleMesh() : verticies({
+                    -1.0, 1.0,
+                    -1.0, -1.0,
+                    1.0, 1.0,
+                    1.0, -1.0
+                    }){}
+
+            ~RectangleMesh() {}
+
+            VecFloat getVerticies() const
+            {
+                return this->verticies;
+            }
+
+            virtual void init() override
+            {
+                this->vao.generate();
+                this->vao.bind();
+
+                // BUFFER CONFIG MUST BE SET!
+
+                this->vbo.generate();
+                this->vbo.bind();
+                this->vbo.setBufferData(this->verticies);
+                this->vbo.setAttribPointers();
+            }
+
+            Ax::GL::VertexArray getVertexArray() 
+            {
+                return this->vao;
+            }
+
+            Ax::GL::VertexBuffer getVertexBuffer()
+            {
+                return this->vbo;
+            }
+
+        private:
+            VecFloat verticies;
+            Ax::GL::VertexArray vao;
+            Ax::GL::VertexBuffer vbo;
+    };
+
+}
+
+
+
+#endif // AX_CUBE_MESH_H
