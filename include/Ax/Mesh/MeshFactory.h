5b1c22e3bd931aacf9c96e5f6d32fb0482c25a60
Luke Hayes <lukepurplehayes@gmail.com>
2022-03-01 18:02:09 +0000

    Added Ax::Mesh::MeshFactory class to project.

---
+++ include/Ax/Mesh/MeshFactory.h
@@ -0,0 +1,36 @@
+#ifndef AX_MESHFACTORY_H
+#define AX_MESHFACTORY_H
+
+#include "Ax/Common/Types.h"
+#include "Ax/GL/VertexArray.h"
+#include <map>
+#include <utility>
+#include <memory>
+
+namespace Ax::Mesh
+{
+    class MeshFactory
+    {
+    public:
+
+        MeshFactory() noexcept;
+        ~MeshFactory();
+
+        /**
+         * Build and load a single Cube Mesh into graphics memory ready for drawing.
+         */
+        void createCubeMesh();
+
+        /**
+         * Build and load a single Rectangle Mesh into graphics memory ready for drawing.
+         */
+        void createRectangleMesh();
+
+
+        std::map<std::string, std::shared_ptr<Ax::GL::VertexArray>> vertexArrays;
+    };
+}
+
+
+
+#endif
---
+++ src/MeshFactory.cpp
@@ -0,0 +1,71 @@
+#include "Ax/Mesh/MeshFactory.h"
+#include "Ax/GL/VertexBuffer.h"
+#include "Ax/Mesh/CubeMesh.h"
+#include "Ax/Mesh/RectangleMesh.h"
+
+
+namespace Ax::Mesh
+{
+    MeshFactory::MeshFactory() noexcept
+    {
+        createCubeMesh();
+        createRectangleMesh();
+    }
+
+    MeshFactory::~MeshFactory()
+    {
+    }
+
+    void MeshFactory::createCubeMesh()
+    {
+        Ax::Mesh::CubeMesh mesh;
+        std::shared_ptr<Ax::GL::VertexArray> vao = std::make_shared<Ax::GL::VertexArray>();
+        vao->generate();
+        vao->bind();
+
+        Ax::GL::VertexBuffer vbo;
+
+        // BUFFER CONFIG MUST BE SET!
+        vbo.config.attributePosition = 0;
+        vbo.config.vertexStride = 0;
+        vbo.config.vertexSize = 3;
+        vbo.config.target = Ax::GL::BufferTarget::ARRAY_BUFFER;
+        vbo.config.primitive = Ax::GL::Primitive::TRIANGLES;
+
+        vbo.generate();
+        vbo.bind();
+        vbo.setBufferData(mesh.getVerticies());
+        vbo.setAttribPointers();
+
+        this->vertexArrays.insert(std::pair<std::string, std::shared_ptr<Ax::GL::VertexArray>>("cube", vao));
+        vao->unbind();
+
+    }
+
+    void MeshFactory::createRectangleMesh()
+    {
+        Ax::Mesh::RectangleMesh mesh;
+        std::shared_ptr<Ax::GL::VertexArray> vao = std::make_shared<Ax::GL::VertexArray>();
+        vao->generate();
+        vao->bind();
+
+        Ax::GL::VertexBuffer vbo;
+
+        // BUFFER CONFIG MUST BE SET!
+        vbo.config.attributePosition = 0;
+        vbo.config.vertexStride = 0;
+        vbo.config.vertexSize = 2;
+        vbo.config.target = Ax::GL::BufferTarget::ARRAY_BUFFER;
+        vbo.config.primitive = Ax::GL::Primitive::TRIANGLE_STRIP;
+
+        vbo.generate();
+        vbo.bind();
+        vbo.setBufferData(mesh.getVerticies());
+        vbo.setAttribPointers();
+
+        this->vertexArrays.insert(std::pair<std::string, std::shared_ptr<Ax::GL::VertexArray>>("rectangle", vao));
+        vao->unbind();
+
+    }
+
+}
