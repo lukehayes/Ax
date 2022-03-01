71d4a2f946b3ba23f9b4b07cf170cda1dc2d6715
Luke Hayes <lukepurplehayes@gmail.com>
2022-03-01 18:03:37 +0000

    Renderer now uses MeshFactory to draw meshes.

--- include/Ax/Renderer/Renderer.h
+++ include/Ax/Renderer/Renderer.h
@@ -8,6 +8,7 @@
 #include "Ax/Mesh/Mesh.h"
 #include "Ax/Entity/Entity.h"
 #include <memory>
+#include "Ax/Mesh/MeshFactory.h"
 
 namespace Ax::Renderer
 {
@@ -17,27 +18,35 @@ namespace Ax::Renderer
         Renderer() {}
         ~Renderer() {}
 
-        void add(std::shared_ptr<Ax::Mesh::CubeMesh>& mesh) 
-        {
-            std::shared_ptr<Ax::Mesh::CubeMesh> m = std::make_shared<Ax::Mesh::CubeMesh>();
-            m->init();
-            this->meshes.push_back(m);
-        }
-
         void drawCube(Ax::Entity::Entity& entity, const Ax::GL::Shader& shader, double x, double y)
         {
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, entity.position);
             model = glm::rotate(model, glm::radians((float)x), {1,1,1});
 
-            std::shared_ptr<Ax::Mesh::CubeMesh> m = meshes.at(0);
-            //m->getVertexArray().bind();
+            std::shared_ptr<Ax::GL::VertexArray> vao = this->meshFactory.vertexArrays["cube"];
+            vao->bind();
             shader.setMat4("model", model);
             shader.setVec3("color", entity.color);
-            glDrawArrays(m->getVertexBuffer().config.primitive, 0, m->getVertexCount());
+            //glDrawArrays(m->getVertexBuffer().config.primitive, 0, m->getVertexCount());
+            glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
-        std::vector<std::shared_ptr<Ax::Mesh::CubeMesh>> meshes;
+        void drawRectangle(Ax::Entity::Entity& entity, const Ax::GL::Shader& shader, double x, double y)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, entity.position);
+            model = glm::rotate(model, glm::radians((float)x), {1,1,1});
+
+            std::shared_ptr<Ax::GL::VertexArray> vao = this->meshFactory.vertexArrays["rectangle"];
+            vao->bind();
+            shader.setMat4("model", model);
+            shader.setVec3("color", entity.color);
+            //glDrawArrays(m->getVertexBuffer().config.primitive, 0, m->getVertexCount());
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 8);
+        }
+
+        Ax::Mesh::MeshFactory meshFactory;
     };
 }
 
