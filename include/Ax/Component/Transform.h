50ff1b0e5fd7434786d95c6f3e077cf79d1385e6
Luke Hayes <lukepurplehayes@gmail.com>
2022-03-02 10:00:33 +0000

    Added Ax::Component::Transform class.

---
+++ include/Ax/Component/Transform.h
@@ -0,0 +1,34 @@
+#ifndef AX_TRANSFORH_H
+#define AX_TRANSFORH_H
+
+#include "Ax/Common/Types.h"
+
+namespace Ax::Component {
+
+    class Transform
+    {
+        public:
+               Transform():
+                   position(0.0f, 0.0f, 0.0f),
+                   rotation(0.0f, 0.0f, 0.0f),
+                   scale(10.0f, 10.0f, 10.0f)
+               {}
+
+               Transform(const V3& position):
+                   position(position),
+                   rotation(0.0f, 0.0f, 0.0f),
+                   scale(10.0f, 10.0f, 10.0f)
+               {}
+
+               ~Transform(){}
+
+            V3 position;
+            V3 rotation;
+            V3 scale;
+            f32 allAxisScale = 10.0f;
+            f32 angle = 0.0;
+            f32 single_scale = 1.0f;
+    };
+}
+
+#endif
