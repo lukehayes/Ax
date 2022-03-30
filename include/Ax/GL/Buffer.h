0e964293691accfcde2db506a117a57d91263c97
Luke Hayes <lukepurplehayes@gmail.com>
2022-03-30 10:30:39 +0100

    Added generic GL::Buffer class.

--- include/Ax/GL/Buffer.h
+++ include/Ax/GL/Buffer.h
@@ -1,14 +1,43 @@
-#ifndef GL_BUFFER_H
-#define GL_BUFFER_H
+#ifndef AX_BUFFER_H
+#define AX_BUFFER_H
 
-namespace GL
+#include "Ax/Common/Types.h"
+#include "Ax/GL/BufferTarget.h"
+#include "Ax/GL/BufferConfig.h"
+
+namespace Ax::GL {
+
+class Buffer
 {
-    class Buffer
-    {
-        Buffer() {}
-        ~Buffer() {}
+public:
 
-    };
+   Buffer();
+   Buffer(
+           const VecFloat& verticies,
+           const VecInt& indicies
+           );
+  ~Buffer();
+
+    void   generateVertexBuffer();
+    void   generateElementBuffer();
+
+    void   bindVertexBuffer();
+    void   bindElementBuffer();
+
+    void   unbindVertexBuffer();
+    void   unbindElementBuffer();
+
+    void   destroyVertexBuffer();
+    void   destroyElementBuffer();
+
+    void   setAttribPointers();
+    void   setBufferData();
+
+    std::vector<f32> verticies;
+    std::vector<size_t> indicies;
+    GLuint vertex_buffer_id;
+    GLuint element_buffer_id;
+};
 }
 
-#endif // GL_BUFFER_H
+#endif
