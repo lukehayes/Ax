a9a91ba09fa98888fcf1206450b9a5009ec02914
lukehayes <lukepurplehayes@gmail.com>
2024-03-05 13:06:10 +0000

    Ax::Entity now using Component::Transform.

--- include/Ax/Component/Transform.h
+++ include/Ax/Component/Transform.h
@@ -5,30 +5,30 @@
 
 namespace Ax::Component {
 
-    class Transform
-    {
-        public:
-               Transform():
-                   position(0.0f, 0.0f, 0.0f),
-                   rotation(0.0f, 0.0f, 0.0f),
-                   scale(10.0f, 10.0f, 10.0f)
-               {}
+class Transform
+{
+public:
+    Transform():
+        position(0.0f, 0.0f, 0.0f),
+        rotation(0.0f, 0.0f, 0.0f),
+        scale(10.0f, 10.0f, 10.0f)
+    {}
 
-               Transform(const V3& position):
-                   position(position),
-                   rotation(0.0f, 0.0f, 0.0f),
-                   scale(10.0f, 10.0f, 10.0f)
-               {}
+    Transform(const V3& position):
+        position(position),
+        rotation(0.0f, 0.0f, 0.0f),
+        scale(10.0f, 10.0f, 10.0f)
+    {}
 
-               ~Transform(){}
+    ~Transform(){}
 
-            V3 position;
-            V3 rotation;
-            V3 scale;
-            f32 allAxisScale = 10.0f;
-            f32 angle = 0.0;
-            f32 single_scale = 1.0f;
-    };
+    V3 position;
+    V3 rotation;
+    V3 scale;
+    f32 allAxisScale = 10.0f;
+    f32 angle = 0.0;
+    f32 single_scale = 1.0f;
+};
 }
 
 #endif
--- include/Ax/Entity/Entity.h
+++ include/Ax/Entity/Entity.h
@@ -1,25 +1,29 @@
 #ifndef AX_ENTITY_H
 #define AX_ENTITY_H
 
-#include "Ax/Common/Types.h"
+#include "Ax/Component/Transform.h"
 
 namespace Ax::Entity
 {
-    class Entity
-    {
-        public:
-            Entity() {}
-            Entity(const glm::vec3& pos) : position(pos) {}
-            Entity(const glm::vec3& pos, const glm::vec3& color)
-                : position(pos),
-                  color(color)
-            {}
-            ~Entity() {}
+class Entity
+{
+public:
 
-            glm::vec3 position = {0,0,0};
-            glm::vec3 color = {0.2,0.2,0.2};
-            double scale = 2.0f;
-    };
+    Entity() : transform() {}
+
+    Entity(const glm::vec3& pos) :
+        transform(pos) {}
+
+    Entity(const glm::vec3& pos, const glm::vec3& color) : 
+        transform(pos),
+        color(color) {}
+
+    ~Entity() {}
+
+    Ax::Component::Transform transform; 
+
+    glm::vec3 color = {0.0, 1.0, 1.0};
+};
 };
 
 
